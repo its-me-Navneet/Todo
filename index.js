23cbe3ad049da2f4dce67b3162b678acb93688c5
its-me-Navneet <87615194+its-me-Navneet@users.noreply.github.com>
2022-07-05 11:49:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+ function update(){ 
+
+  itemjsonarraystr = localStorage.getItem('itemsJson') ;
+        itemjsonarray = JSON.parse(itemjsonarraystr) ;
+        localStorage.setItem('itemsJson' , JSON.stringify(itemjsonarray)) ;
+
+  let tablebody = document.getElementById("tablebody") ; 
+
+  let str="" ;
+
+  itemjsonarray.forEach((element,index ) => {
+      str+=`
+      <tr>
+              <td>${index+1}</td>
+              <td>${element[0]}</td>
+              <td>${element[1]}</td> 
+               <td><button type="submit" onclick="deletee(${index})" >Remove</button></td>
+          </tr>
+          `
+  });
+  tablebody.innerHTML=str ;
+
+ }
+ function add() { 
+     
+    let tit= document.getElementById("title").value;
+     let desc =document.getElementById("Description").value;  
+     //console.log(tit) ;
+    if(localStorage.getItem('itemsJson')==null){ 
+        console.log("hh");
+        itemjsonarray =[] ;
+        itemjsonarray.push([tit,desc]) ; 
+        localStorage.setItem('itemsJson',JSON.stringify(itemjsonarray)) ;
+    }
+    else{
+        itemjsonarraystr = localStorage.getItem('itemsJson') ;
+        itemjsonarray = JSON.parse(itemjsonarraystr) ;
+        itemjsonarray.push([tit,desc]) ;
+        localStorage.setItem('itemsJson' , JSON.stringify(itemjsonarray)) ;
+
+    } 
+ 
+
+    let tablebody = document.getElementById("tablebody") ; 
+
+    let str="" ;
+
+    itemjsonarray.forEach((element,index ) => {
+        str+=`
+        <tr>
+                <td>${index+1}</td>
+                <td>${element[0]}</td>
+                <td>${element[1]}</td> 
+                 <td><button type="submit" onclick="deletee(${index})" >Remove</button></td>
+            </tr>
+            `
+    });
+    tablebody.innerHTML=str ;
+  
+
+ }  
+ var cli =document.getElementById("add") ;
+ cli.addEventListener("click",add); 
+   update();
+ function deletee(index){
+    itemjsonarraystr = localStorage.getItem('itemsJson') ;
+    itemjsonarray = JSON.parse(itemjsonarraystr) ;  
+     itemjsonarray.splice(index,1) ;
+    localStorage.setItem('itemsJson' , JSON.stringify(itemjsonarray)) ; 
+     update();
+ }
+ 
